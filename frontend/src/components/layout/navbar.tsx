093d8209a6fd000e97057dc68e719907ce2bd12a
Ryan Le <31494954+lryanle@users.noreply.github.com>
2023-10-27 05:04:47 -0500

    whoops broke everything

--- frontend/src/components/layout/navbar.tsx
+++ frontend/src/components/layout/navbar.tsx
@@ -31,21 +31,23 @@ export default function Navbar({
       </button>
       {status !== 'loading' &&
         (session?.user ? (
-          <Link href={`/${session.user?.name}`} className="flex items-center justify-center gap-2" legacyBehavior>
-            <span className="text-md">{session.user?.name}</span>
-            <a className="w-8 h-8 rounded-full overflow-hidden">
-              <img
-                src={
-                  session.user?.image ||
-                  `https://avatar.tobi.sh/${session.user.name}`
-                }
-                alt={session.user?.name || 'User'}
-                width={300}
-                height={300}
-                // placeholder="blur"
-                // blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVQYV2PYsGHDfwAHNAMQumvbogAAAABJRU5ErkJggg=="
-              />
-            </a>
+          <Link href={`/${session.user?.name}`} legacyBehavior>
+            <div className="flex items-center justify-center gap-2">
+              <span className="text-md">{session.user?.name}</span>
+              <a className="w-8 h-8 rounded-full overflow-hidden">
+                <img
+                  src={
+                    session.user?.image ||
+                    `https://avatar.tobi.sh/${session.user.name}`
+                  }
+                  alt={session.user?.name || 'User'}
+                  width={300}
+                  height={300}
+                  // placeholder="blur"
+                  // blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVQYV2PYsGHDfwAHNAMQumvbogAAAABJRU5ErkJggg=="
+                />
+              </a>
+            </div>
           </Link>
         ) : (
           <button

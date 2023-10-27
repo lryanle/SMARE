ef58802be059bf3d0c4b041b463a9928bd7ba1db
waseem-polus <waseem_imad@yahoo.com>
2023-10-26 22:27:02 -0500

    replaced spaces with tabs

--- src/scrapers/craigslist/homepage.py
+++ src/scrapers/craigslist/homepage.py
@@ -6,112 +6,112 @@ import time
 from datetime import date
 
 def scrollTo(x, driver):
-    driver.execute_script(f"window.scrollTo({{top: {x}, left: 100, behavior: 'smooth'}})")
+	driver.execute_script(f"window.scrollTo({{top: {x}, left: 100, behavior: 'smooth'}})")
 
 def loadPageResources(driver):
-    scroll = 100
+	scroll = 100
 
-    print("Waiting to load...")
-    time.sleep(2)
+	print("Waiting to load...")
+	time.sleep(2)
 
-    scrollTo(scroll, driver)
+	scrollTo(scroll, driver)
 
-    loadImgButtons = driver.find_elements("class name", "slider-back-arrow")
+	loadImgButtons = driver.find_elements("class name", "slider-back-arrow")
 
-    time.sleep(2)
+	time.sleep(2)
 
-    # Emulate a user scrolling
-    for i in range(len(loadImgButtons)):
-        scroll += 100
-        scrollTo(scroll, driver)
+	# Emulate a user scrolling
+	for i in range(len(loadImgButtons)):
+		scroll += 100
+		scrollTo(scroll, driver)
 
-        driver.execute_script("arguments[0].click();", loadImgButtons[i])
+		driver.execute_script("arguments[0].click();", loadImgButtons[i])
 
-        time.sleep(.5)
+		time.sleep(.5)
 
 
 def setupURLs():
-    #list of cities to scrape; can be expanded
-    cities = ["abilene", "amarillo", "austin", "beaumont", "brownsville", "collegestation", "corpuschristi", "dallas", "nacogdoches", "delrio", "elpaso", "galveston", "houston", "killeen", "laredo", "lubbock", "mcallen", "odessa", "sanangelo", "sanantonio", "sanmarcos", "bigbend", "texoma", "easttexas", "victoriatx", "waco", "wichitafalls"]
+	#list of cities to scrape; can be expanded
+	cities = ["abilene", "amarillo", "austin", "beaumont", "brownsville", "collegestation", "corpuschristi", "dallas", "nacogdoches", "delrio", "elpaso", "galveston", "houston", "killeen", "laredo", "lubbock", "mcallen", "odessa", "sanangelo", "sanantonio", "sanmarcos", "bigbend", "texoma", "easttexas", "victoriatx", "waco", "wichitafalls"]
 
-    oldestAllowedCars = 2011
+	oldestAllowedCars = 2011
 
-    # Set the URL of the Facebook Marketplace automotive category
-    base_url = 'https://{}.craigslist.org/search/cta?min_auto_year={}#search=1~gallery~0~0'
-    return [base_url.format(city, oldestAllowedCars) for city in cities]
+	# Set the URL of the Facebook Marketplace automotive category
+	base_url = 'https://{}.craigslist.org/search/cta?min_auto_year={}#search=1~gallery~0~0'
+	return [base_url.format(city, oldestAllowedCars) for city in cities]
 
 def setupBrowser():
-    print("Setting up headless browser")
+	print("Setting up headless browser")
 
-    options = Options()
-    options.add_argument("--headless=new")
+	options = Options()
+	options.add_argument("--headless=new")
 
-    print("Creating a new Selenium WebDriver instance")
-    return webdriver.Chrome(options=options)
+	print("Creating a new Selenium WebDriver instance")
+	return webdriver.Chrome(options=options)
 
 def getAllPosts(browser):
-    # Create a BeautifulSoup object from the HTML of the page
-    html = browser.page_source
-    soup = BeautifulSoup(html, 'html.parser')
+	# Create a BeautifulSoup object from the HTML of the page
+	html = browser.page_source
+	soup = BeautifulSoup(html, 'html.parser')
 
-    # Find all of the car listings on the page
-    return soup.find_all('div', class_='gallery-card')
+	# Find all of the car listings on the page
+	return soup.find_all('div', class_='gallery-card')
 
 def getCarImages():
-    return "TODO"
+	return "TODO"
 
 def getCarInfo(post):
-    title = post.find('span', class_='label').text
+	title = post.find('span', class_='label').text
 
-    print(f'Scraping "{title}"')
+	print(f'Scraping "{title}"')
 
-    price = post.find('span', class_='priceinfo').text
-    metadata = post.find('div', class_="meta").text.split('·')
+	price = post.find('span', class_='priceinfo').text
+	metadata = post.find('div', class_="meta").text.split('·')
 
-    miles = metadata[1]
-    if (len(metadata) >= 3):
-        location = metadata[2]
-    
-    link = post.find('a', class_='posting-title', href=True)["href"]
-    
-    imageElements = post.findAll('img')
-    images = [img["src"] for img in imageElements]
+	miles = metadata[1]
+	if (len(metadata) >= 3):
+		location = metadata[2]
+	
+	link = post.find('a', class_='posting-title', href=True)["href"]
+	
+	imageElements = post.findAll('img')
+	images = [img["src"] for img in imageElements]
 
-    return {
-        "title": title, 
-        "price": price, 
-        "location": location, 
-        "miles": miles, 
-        "link": link,
-        "images": images,
-        "scrapeDate": date.today()
-    }
+	return {
+		"title": title, 
+		"price": price, 
+		"location": location, 
+		"miles": miles, 
+		"link": link,
+		"images": images,
+		"scrapeDate": date.today()
+	}
 
 def scrapeHomepage():
-    cityURLs = setupURLs()
-    browser = setupBrowser()
+	cityURLs = setupURLs()
+	browser = setupBrowser()
 
-    for url in cityURLs:
-        # Navigate to the URL
-        print(f"Going to {url}")
-        browser.get(url) 
+	for url in cityURLs:
+		# Navigate to the URL
+		print(f"Going to {url}")
+		browser.get(url) 
 
-        print(f"Loading cars from {url}")
+		print(f"Loading cars from {url}")
 
-        loadPageResources(browser)
+		loadPageResources(browser)
 
-        carPosts = getAllPosts(browser)
+		carPosts = getAllPosts(browser)
 
-        # Iterate over the listings and scrape the data
-        for post in carPosts:
-            try:
-                car = getCarInfo(post)
-                print(car)
-            except:
-                print("Incomplete listing info")
-                
-    # Close the Selenium WebDriver instance
-    browser.quit()
+		# Iterate over the listings and scrape the data
+		for post in carPosts:
+			try:
+				car = getCarInfo(post)
+				print(car)
+			except:
+				print("Incomplete listing info")
+				
+	# Close the Selenium WebDriver instance
+	browser.quit()
 
 if (__name__ == "__main__"):
-    scrapeHomepage()
+	scrapeHomepage()

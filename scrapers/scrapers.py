f1298de3adc3a27947040eb2171fbb1247934635
waseem-polus <waseem_imad@yahoo.com>
2023-10-31 11:40:56 -0500

    Extracted scraper logic into utils

--- scrapers/scrapers.py
+++ scrapers/scrapers.py
@@ -2,65 +2,26 @@ from typing import Optional
 from typing_extensions import Annotated
 import typer
 
-import craigslist as cl
-import facebook as fb
-import database as db
+import craigslist
+import facebook
 import utils
 
 app = typer.Typer()
 
 @app.command()
 def craigslist():
-	cityURLs = cl.setupURLs(2011)
-	browser = utils.setupBrowser()
-
-	for url in cityURLs:
-		print(f"Going to {url}")
-		browser.get(url) 
-
-		print(f"Loading cars from {url}")
-		cl.loadPageResources(browser)
-
-		carPosts = cl.getAllPosts(browser)
-
-		for post in carPosts:
-			try:
-				title, price, location, odometer, link, images = cl.getCarInfo(post)
-				db.post_raw("craigslist", title, price, location, odometer, link, images)
-			except Exception as error:
-				print(error)
-				
-	browser.quit()
+	utils.scrape(craigslist, "craigslist")
 
 @app.command()
 def facebook():
-	cityURLs = fb.setupURLs(2011)
-	browser = utils.setupBrowser()
-
-	for url in cityURLs:
-		print(f"Going to {url}")
-		browser.get(url) 
-
-		print(f"Loading cars from {url}")
-		fb.loadPageResources(browser)
-
-		carPosts = fb.getAllPosts(browser)
-
-		for post in carPosts:
-			try:
-				title, price, location, odometer, link, images = fb.getCarInfo(post)
-				db.post_raw("facebook", title, price, location, odometer, link, images)
-			except Exception as error:
-				print(error)
-				
-	browser.quit()
+	utils.scrape(facebook, "facebook")
 
 @app.command()
 def link(link: str):
 	if (".craigslist.org" in link):
-		cl.scrapeListing(link)
+		craigslist.scrapeListing(link)
 	elif("https://www.facebook.com/marketplace" in link):
-		print("facebook marketplace")
+		facebook.scrapeListing(link)
 	else:
 		print("Not a Craigslist nor a Facebook Marketplace link")
 
--- scrapers/utils.py
+++ scrapers/utils.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+import utils
+import database as db
 
 def scrollTo(x, driver):
 	driver.execute_script(f"window.scrollTo({{top: {x}, left: 100, behavior: 'smooth'}})")
@@ -12,3 +14,25 @@ def setupBrowser():
 
 	print("Creating a new Selenium WebDriver instance")
 	return webdriver.Chrome(options=options)
+
+def scrape(scraper, website):
+	cityURLs = scraper.setupURLs(2011)
+	browser = utils.setupBrowser()
+
+	for url in cityURLs:
+		print(f"Going to {url}")
+		browser.get(url) 
+
+		print(f"Loading cars from {url}")
+		scraper.loadPageResources(browser)
+
+		carPosts = scraper.getAllPosts(browser)
+
+		for post in carPosts:
+			try:
+				title, price, location, odometer, link, images = scraper.getCarInfo(post)
+				db.post_raw(website, title, price, location, odometer, link, images)
+			except Exception as error:
+				print(error)
+				
+	browser.quit()

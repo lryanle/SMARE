39048333868d25494a085cf5d37b5b2308ac9e1d
Ryan Le <31494954+lryanle@users.noreply.github.com>
2023-11-10 12:52:19 -0600

    fixed linkedin

--- frontend/app/api/auth/[...nextauth]/authOptions.ts
+++ frontend/app/api/auth/[...nextauth]/authOptions.ts
@@ -64,6 +64,21 @@ export const authOptions: NextAuthOptions = {
       //     image: profile.avatar_url
       //   };
       // },
+      authorization: {
+        params: { scope: 'openid profile email' },
+      },
+      issuer: 'https://www.linkedin.com',
+      jwks_endpoint: 'https://www.linkedin.com/oauth/openid/jwks',
+      profile(profile, tokens) {
+        const defaultImage =
+          'https://cdn-icons-png.flaticon.com/512/174/174857.png';
+        return {
+          id: profile.sub,
+          name: profile.name,
+          email: profile.email,
+          image: profile.picture ?? defaultImage,
+        };
+      },
     }),
     // CredentialsProvider({
     //   name: "credentials",

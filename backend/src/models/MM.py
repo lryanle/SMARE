d13dd62b7c682fbaf28cb007d74eed31f01a2d8e
waseem-polus <waseem_imad@yahoo.com>
2024-03-14 17:17:42 -0500

    refactoring models (co-authored by ryan)

--- backend/src/models/MM.py
+++ backend/src/models/MM.py
@@ -1,18 +1,18 @@
 # Importing the M3_riskscores and M4_riskscores functions from their respective modules
-import M3
-import M4
+from M3 import M3_riskscores
+from M4 import M4_riskscores
 import pandas as pd
 
-
+# todo: pass in data from mongo through each function
+# todo: calculate post-weight-product scores here, and not in each individual function.
 def model_manager():
-    # Calling M3_riskscores function to get results from Model M3
-    m3_results = M3.M3_riskscores()
-    # Calling M4_riskscores function to get results from Model M4
-    m4_results = M4.M4_riskscores()
+    m3_results = M3_riskscores()
+    m4_results = M4_riskscores()
+
     # Merging the results from both models based on a common identifier, if applicable
     merged_results = pd.merge(m3_results, m4_results, on='_id', how='inner')
     # Perform any additional data processing or calculations to derive the final risk score
-    final_risk_score = (merged_results['risk_score_M3'] + merged_results['risk_score_M4_y']) / 2
+    final_risk_score = min(100,merged_results['risk_score_M3'] + merged_results['risk_score_M4_y'])
     # Add the final risk score to the merged results DataFrame
     merged_results['final_risk_score'] = final_risk_score
 
@@ -24,9 +24,4 @@ def model_manager():
     # Returning the list of tuples containing 'id' and 'final_risk_score'
     return output.values.tolist()
 
-
-# Call the model_manager function to execute the model management process
-final_results = model_manager()
-
-# Print the final results
-print(final_results)
+print(model_manager())

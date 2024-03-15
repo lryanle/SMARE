81b7becf66835252b32e29a4a8ab3c8be2851fb3
Temitayo Aderounmu <91757922+temitayoaderounmu@users.noreply.github.com>
2024-03-15 15:31:49 -0500

    Update model_cleaner.py

--- backend/src/models/model_cleaner.py
+++ backend/src/models/model_cleaner.py
@@ -21,23 +21,12 @@ print(f"Data has been successfully exported to {csv_filename}")
 cars = pd.read_csv("output_data.csv")
 
 # DATA CLEANING
-# drop unnecessary variables: id, url
-cars = cars.drop(
-    columns=[
-        "source",
-        "link",
-        "location",
-        "scraper-version",
-        "scrape-date",
-        "images",
-        "postBody",
-        "latitude",
-        "longitude",
-        "year",
-        "attributes",
-        "makeModel",
-    ]
-)
+# drop unnecessary variables
+# Look at the number of NA values by column
+na_counts = cars.isna().sum()
+# Drop columns with NA values greater than 15
+cols_to_drop = na_counts[na_counts > 15].index
+cars = cars.drop(columns=cols_to_drop)
 
 # drop NA values
 cars = cars.dropna()
@@ -50,14 +39,13 @@ cars["odometer"] = cars["odometer"].apply(
 cars["year"] = cars["title"].str.extract(r"(\d{4})")
 # Fill NaN values in the 'year' column with a placeholder
 cars["year"].fillna(-1, inplace=True)
-cars = cars.astype({"year": "int", "odometer": "int"})
-
+# Drop rows with NaN values in the 'odometer' column
+cars = cars.dropna(subset=['odometer'])
+# Convert 'odometer' column to integers
+cars['odometer'] = cars['odometer'].astype('int')
 # create an age variable to get a better understanding of how old a car is
 cars["age"] = datetime.date.today().year - cars["year"]
 
-cars = cars[cars.year >= 2005]
-
-ars = cars[cars.odometer <= 300]
 # Clean up the 'price' column by replacing non-numeric values with NaN
 cars["price"] = pd.to_numeric(
     cars["price"].replace("[$,]", "", regex=True), errors="coerce"

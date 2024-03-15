7494e3ac22503405b79a7d16e59bc54da17d7007
waseem-polus <waseem_imad@yahoo.com>
2024-03-15 15:38:22 -0500

    checkpoint facebook model extractor

--- backend/src/cleaners/facebook.py
+++ backend/src/cleaners/facebook.py
@@ -1,4 +1,7 @@
 import re
+import json
+
+from difflib import get_close_matches
 
 # Define patterns for different attributes
 attrPatterns = {
@@ -48,3 +51,43 @@ def extract_year(title_str):
         return None
 
     return int(year.group(0))
+
+def extract_model_wreg(title, make):
+    with open("kbb_data.json") as kbbjson:
+        models = json.load(kbbjson)
+
+    # Use regex to find patterns like "2021 RAM 3500" in the title
+    match = re.search(r"\b\d{4}\s*[a-zA-Z0-9-]+\s*([a-zA-Z0-9-]+)\b", title)
+    
+    if not match:
+        return None
+    
+    model = match.group(1)
+    
+    if model is not None:
+        # Handle models with dashes
+        model = model.replace("-", "")
+        # Compare with the kbb_models dictionary
+        matched_model = get_close_matches(model, models[make], n=1)
+        if matched_model:
+            return matched_model[0]
+        else:
+            # If no direct match, try finding a close match using pieces of words
+            title_words = re.findall(r"\b\w+\b", title)
+            extracted_model_pieces = []
+            for word in title_words:
+                # Check if the word is part of the make name, if yes, skip it
+                if make is not None and word.lower() in make.lower():
+                    continue
+                extracted_model_pieces.append(word)
+                current_model_attempt = " ".join(extracted_model_pieces)
+                # Check if the current attempt is a close match
+                matched_model = get_close_matches(
+                    current_model_attempt, models[make], n=1
+                )
+                if matched_model:
+                    return matched_model[0]
+            # If still no match, return the original extracted model
+            return model
+    else:
+        return None

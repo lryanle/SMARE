90993c14110915f838caf0b8787c0cb4336c4461
Temitayo Aderounmu <91757922+temitayoaderounmu@users.noreply.github.com>
2024-03-29 10:54:28 -0500

    Update m4_carfreq.py

--- backend/src/models/m4_carfreq.py
+++ backend/src/models/m4_carfreq.py
@@ -4,34 +4,38 @@ from ..utilities import logger
 # Initialize logger
 logger = logger.SmareLogger()
 
-def m4_riskscores(listings):
-    logger.info("Starting M4 model for calculating risk scores...")
-    output = []
+def m4_riskscores(car_listings):
+    try:
+        if not isinstance(car_listings, list):
+            car_listings = [car_listings] 
+            logger.warning("Model 4: Input is not a list. Converting to a list.")
+        
+        if len(car_listings) == 0:
+            logger.error("Model 4: Input list is empty.")
+            return []
 
-    for k, listing in enumerate(listings):
-        try:
-            logger.debug(f"Model 4: Processing listing {k + 1}/{len(listings)}")
-            model = listing.get('model')
+        all_models = [data.get('model') for data in car_listings]
+        models_df = pd.DataFrame(all_models, columns=['model'])
+        model_frequency = models_df['model'].value_counts()
+        max_frequency = model_frequency.max()
+        risk_scores = []
+        processed_listings = 0  
 
-            if model is not None:
-                all_models = [listing['model'] for listing in listings]
-                models_df = pd.DataFrame(all_models, columns=['model'])
-                model_frequency = models_df['model'].value_counts()
-                max_frequency = model_frequency.max()
+        for model, frequency in model_frequency.items():
+            try:
+                logger.debug(f"Model 4: Processing listing {processed_listings + 1}/{len(car_listings)}")
+                processed_listings += frequency
+                risk_score = frequency / max_frequency
+                risk_score = max(0, min(1, risk_score))
+                risk_scores.extend([risk_score] * frequency)
+            except Exception as e:
+                logger.error(f"Model 4: Error processing listing: {e}")
 
-                risk_scores = []
-                for model, frequency in model_frequency.items():
-                    risk_score = frequency / max_frequency            
-                    risk_score = max(0, min(1, risk_score))
-                    risk_scores.append(risk_score)
-                output.append(risk_scores)
-            else:
-                logger.warning("Model information not found in the listing.")
-                output.append([])
-        except Exception as e:
-            logger.warning(f"Error with model 4: {e}")
-            output.append(-1)
-            continue
-
-    logger.info("M4 model execution completed successfully.")
-    return output
+        if len(car_listings) != processed_listings:
+            logger.error("Model 4: Input and output array sizes do not match.")
+            return [-1] * len(car_listings)  
+        return risk_scores
+            
+    except Exception as e:
+        logger.error(f"Model 4: Error in M4 model: {e}")
+        return [-1] * len(car_listings)  

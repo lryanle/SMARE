abf8d47966df16da35c76a2e24025a34db1b943e
Waseem Polus <69316929+waseem-polus@users.noreply.github.com>
2024-04-05 02:41:06 -0500

    Added proxies

--- backend/src/scrapers/utils.py
+++ backend/src/scrapers/utils.py
@@ -1,7 +1,12 @@
 import time
+import os
+
+from ..utilities import logger
+from .extension import proxies
 
 from selenium import webdriver
 
+logger = logger.SmareLogger()
 
 def scroll_to(x, driver):
     driver.execute_script(
@@ -13,11 +18,10 @@ def click_on(elem, driver):
     driver.execute_script("arguments[0].click();", elem)
 
 
-def create_driver_options():
+def create_driver_options(use_proxy=False):
     options = webdriver.ChromeOptions()
     options.binary_location = "/opt/chrome/chrome"
 
-    options.add_argument("--headless=new")
     options.add_argument("--headless=new")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-gpu")
@@ -27,23 +31,34 @@ def create_driver_options():
     options.add_argument("--disable-dev-tools")
     options.add_argument("--no-zygote")
 
+    if use_proxy:
+        logger.info("Setting up proxy")
+
+        username = os.getenv("PROXY_USERNAME")
+        password = os.getenv("PROXY_PASSWORD")
+        port = os.getenv("PROXY_PORT")
+        ENDPOINT = 'gate.smartproxy.com'
+
+        proxies_extension = proxies(username, password, ENDPOINT, port)
+        options.add_extension(proxies_extension)
+
     return options
 
 
-def setup_browser():
-    print("Setting up headless browser")
+def setup_browser(use_proxy=False):
+    logger.info("Setting up headless browser")
 
     service = webdriver.ChromeService("/opt/chromedriver")
-    options = create_driver_options()
+    options = create_driver_options(use_proxy)
 
-    print("Creating a new Selenium WebDriver instance")
+    logger.info("Creating a new Selenium WebDriver instance")
     return webdriver.Chrome(options=options, service=service)
 
 
 def load_page_resources(driver):
     scroll = 1000
 
-    print("Waiting to load...")
+    logger.info("Waiting to load...")
     time.sleep(2)
     scroll_to(scroll, driver)
     time.sleep(2)
